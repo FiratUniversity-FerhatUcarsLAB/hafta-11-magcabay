6c5ba3cae0cf387e022ac80c2f6dc4bb02fc0fcc
Miraç Ö. AĞCABAY <miracagcabayy@gmail.com>
2025-12-03 21:35:19 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,20 @@
 public class Exercise4_2 {
-
-  // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
-
-    public static void main(String[] args) {
-        zippo("rattle", 13);
+    public static void main(String[] args) {          // 1
+        zippo("rattle", 13);              // 2
     }
 
-    public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
+    public static void baffle(String blimp) {         // 6
+        System.out.println(blimp);                    // 7
+        zippo("ping", -5);                // 8
     }
 
-    public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
-        } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+    public static void zippo(String quince, int flag) { // 3, 9
+        if (flag < 0) {                               // 10
+            System.out.println(quince + " zoop");     // 11
+        } else {                                      // 4
+            System.out.println("ik");                 // 5
+            baffle(quince);                           // 6
+            System.out.println("boo -wa-ha-ha");      // 12
         }
     }
 }
