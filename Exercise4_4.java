b65ea6edde0ec6b1d3e5aa1ddee3a2e0d7b37406
Miraç Ö. AĞCABAY <miracagcabayy@gmail.com>
2025-12-03 21:48:01 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -11,11 +11,32 @@ public class Exercise4_4 {
     public static void main(String[] args) {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
-        getNumber();
+        /* Derleyici UYARI VERMEZ ve HATA VERMEZ.
+     * Program normal çalışır, ancak dönen değer kaybolur.
+     * Bu kötü programlama pratiğidir ama Java buna izin verir.
+     * 
+     * Örnek:
+     * returnMethod();  // 42 döner ama hiçbir yerde kullanılmaz
+        */
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+      /*  DERLEME HATASI (Compile Error) verir!
+      
+      Hata Mesajı:
+      "error: 'void' type not allowed here"
+      
+      Açıklama:
+      void metotlar hiçbir değer döndürmez. Bu yüzden matematiksel
+      işlemlerde, System.out.println() içinde veya herhangi bir
+      ifadenin parçası olarak kullanılamazlar.
+      
+      Hatalı: System.out.println(voidMethod() + 7);
+      Doğru:  voidMethod();
+              System.out.println(7);
+     */
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
 }
+
